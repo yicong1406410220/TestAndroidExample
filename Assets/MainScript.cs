6c6c1377383b98d3a9739b977419a4c9a8bc9750
yicong1406410220 <azazaz123456@foxmail.com>
2020-04-26 00:12:50 +0800

    安卓jar包例子

---
+++ Assets/MainScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScript : MonoBehaviour
+{
+#if UNITY_ANDROID// && !UNITY_EDITOR
+    AndroidJavaObject currActivity;
+#endif
+    //UI组件
+    Button button;
+    InputField inputField;
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_ANDROID// && !UNITY_EDITOR
+        //保存当前安卓活动的引用
+        currActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+#endif
+        //获取界面上的UI实例
+        button = transform.Find("Button").GetComponent<Button>();
+        inputField = transform.Find("InputField").GetComponent<InputField>();
+        text = transform.Find("Text").GetComponent<Text>();
+        //按钮添加点击响应
+        button.onClick.AddListener(Hello);
+    }
+
+    public void Hello()
+    {
+#if UNITY_ANDROID// && !UNITY_EDITOR
+        //调用安卓里面对应的hello方法
+        currActivity.Call("Hello", inputField.text);
+#endif
+    }
+
+    // 等待安卓java代码调用的方法
+    public void Accept(string str)
+    {
+        text.text = str;
+    }
+}
